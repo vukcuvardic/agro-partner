6a8871c27f7383bb982c8e9a3f98ed75c96848da
mr.wolf <vukcuvardic@gmail.com>
2015-07-24 12:16:32 +0200

    new computer php

--- single-product.php
+++ single-product.php
@@ -1,11 +1,11 @@
 <?php include '/partials/header.php'; ?>
 <!-- Main Container -->
 <div class="main-container">
-	<!-- Main Content -->
-	<main>
-		<?php include '/partials/single-product.php'; ?> 
-	</main>
-	<!-- Sidebar -->
-	<?php include '/partials/sidebar.php'; ?> 
+    <!-- Main Content -->
+    <main>
+        <?php include '/partials/single-product.php'; ?> 
+    </main>
+    <!-- Sidebar -->
+    <?php include '/partials/sidebar.php'; ?> 
 </div><!--- End Main Container -->
 <?php include '/partials/footer.php'; ?>
